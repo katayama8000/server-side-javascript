223336908437d6a03a8080246a1681731e931fde
katayama8000 <tattu.0310@gmail.com>
2023-01-19 13:25:25 +0900

    feat:404

--- firebase-admin-nest/src/app.controller.ts
+++ firebase-admin-nest/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('error')
+  error404() {
+    return this.appService.error404();
+  }
 }
--- firebase-admin-nest/src/app.service.ts
+++ firebase-admin-nest/src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  error404() {
+    throw new NotFoundException('404test');
   }
 }
