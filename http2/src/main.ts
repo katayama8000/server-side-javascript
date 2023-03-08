eb97a90be4762314065aeb20ef4ab7fb11b73eb6
katayama8000 <tattu.0310@gmail.com>
2023-03-08 18:07:04 +0900

    http2

--- http2/src/main.ts
+++ http2/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import spdy = require('spdy');
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express = require('express');
 import { AppModule } from './app.module';
 
+const options = {
+  spdy: {
+    protocols: ['h2', 'http/1.1'],
+    plain: true, // h2cの場合はtrue
+  },
+};
+
+const expressApp = express();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+  await app.init();
+
+  const server = spdy.createServer(options, expressApp);
+  server.listen(3000);
 }
 bootstrap();
