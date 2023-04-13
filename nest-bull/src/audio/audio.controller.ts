a20ad109d3d734f1f3c23aa33d5cfcedc2a7395e
katayama8000 <tattu.0310@gmail.com>
2023-04-13 22:18:16 +0900

    bull

--- nest-bull/src/audio/audio.controller.ts
+++ nest-bull/src/audio/audio.controller.ts
@@ -4,8 +4,13 @@ import { AudioService } from './audio.service';
 @Controller('audio')
 export class AudioController {
   constructor(private readonly audioService: AudioService) {}
-  @Get('/')
-  async getAudio() {
-    return await this.audioService.getAudio();
-  }
+  //   @Get('/')
+  //   async getAudio() {
+  //     return await this.audioService.getAudio();
+  //   }
+
+  //   @Get('/add')
+  //   async addAudio() {
+  //     return await this.audioService.addAudio();
+  //   }
 }
--- nest-bull/src/audio/audio.service.ts
+++ nest-bull/src/audio/audio.service.ts
@@ -1,8 +1,18 @@
+import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
+import { Queue } from 'bull';
 
 @Injectable()
 export class AudioService {
+  //   constructor(@InjectQueue('audio') private audioQueue: Queue) {}
   async getAudio() {
     return 'audio';
+  }
+
+  async addAudio() {
+    // const job = await this.audioQueue.add({
+    //   foo: 'bar',
+    // });
+    // console.log(job);
   }
 }
