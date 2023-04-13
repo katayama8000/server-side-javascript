f3e9710cc37bb470ac2e81ab635061fe0f20a552
katayama8000 <tattu.0310@gmail.com>
2023-04-13 21:45:29 +0900

    delete

--- nest-bull/src/bull/bull.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BullController } from './bull.controller';
-import { BullService } from './bull.service';
-
-@Module({
-  controllers: [BullController],
-  providers: [BullService]
-})
-export class BullModule {}
--- nest-bull/src/bull/bull.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class BullService {
-  getBull() {
-    return 'This action returns all bull';
-  }
-}
