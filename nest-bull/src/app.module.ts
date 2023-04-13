cb818008b975e926d36635f90b3437956e2c97a1
katayama8000 <tattu.0310@gmail.com>
2023-04-13 22:03:09 +0900

    audio

--- nest-bull/src/app.module.ts
+++ nest-bull/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BullModule } from './bull/bull.module';
+import { BullModule } from '@nestjs/bull';
+import { AudioModule } from './audio/audio.module';
 
 @Module({
-  imports: [BullModule],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+      limiter: {
+        max: 1,
+        duration: 1000,
+      },
+    }),
+    AudioModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ nest-bull/src/audio/audio.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { AudioService } from './audio.service';
+
+@Controller('audio')
+export class AudioController {
+  constructor(private readonly audioService: AudioService) {}
+  @Get('/')
+  async getAudio() {
+    return await this.audioService.getAudio();
+  }
+}
